317bc168943008e637d2cd6fc5dfa314821dbf93
Earlz <earlz@lastyearswishes.com>
2014-04-17 21:25:19 -0400

    Update default ports to not conflict

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -111,7 +111,7 @@ public:
         pchMessageStart[3] = 0xc0;
         //currently just invalid pubkey
         vAlertPubKey = ParseHex("0554da7a5dae4db797d9b0644d57a5cd50e05a70f36091cd62e2fc41c98ded06340be5a43a35e185690cd9cde5d72da8f6d065b499b06f51dcfba14aad859f443a");
-        nDefaultPort = 22556;
+        nDefaultPort = 22889;
         nRPCPort = 22888;
         bnProofOfWorkLimit = CBigNum(~uint256(0) >> 20);
         nSubsidyHalvingInterval = 210000;
@@ -206,7 +206,7 @@ public:
         pchMessageStart[2] = 0xb7;
         pchMessageStart[3] = 0xdc;
         vAlertPubKey = ParseHex("042756726da3c7ef515d89212ee1705023d14be389e25fe15611585661b9a20021908b2b80a3c7200a0139dd2b26946606aab0eef9aa7689a6dc2c7eee237fa834");
-        nDefaultPort = 44556;
+        nDefaultPort = 44889;
         nRPCPort = 44888;
         strDataDir = "testnet3";
 
